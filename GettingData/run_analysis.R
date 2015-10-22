3865c9a84e2a5258eba27cf9dbdc8e8227a8a5b5
msibomana <sibomana@gmail.com>
2015-10-22 09:45:24 +0200

    Update run_analysis.R

--- GettingData/run_analysis.R
+++ GettingData/run_analysis.R
@@ -1,6 +1,17 @@
 # Getting and Cleaning Data project
 # run_analysis.R script 
 # Author: MSibomana
+#
+# usage
+#   data4 <- read_data4(datadir) : reads training and test datasets from user provided directory
+#                                  and returns one merged dataset.
+#   data5 <- make_data5(data4) : Creates a new dataset with averages from data4 of each variable 
+#                                for each activity and each subject
+#   data5 <- read_data5(datadir) : calls make_data5(read_data4(datadir))
+#
+#   write_data5(data5, filename): writes data5 to filename in text format without row names
+#
+#   read_write_data5(datadir, filename): calls write_data5(read_data5(datadir),filename)
 
 library(data.table)
 library(dplyr)
@@ -28,16 +39,26 @@ read_data4 <- function(datadir) {
     train <- read_dataset(datadir,"train",lbl$V2)
     test <- read_dataset(datadir,"test",lbl$V2)
     data4 <- bind_rows(train, test)
+    # convert Subject,Activity and Dataset to factors
+    data4$Subject <- as.factor(data4$Subject)
+    data4$Activity <- as.factor(data4$Activity)
+    data4$Dataset <- as.factor(data4$Dataset)
+    data4
 }
 
 make_data5 <- function(data4) {
-    criteria <-list(Dataset=as.factor(data4$Dataset),
-                Subject=as.factor(data4$Subject),
-                Activity=as.factor(data4$Activity))
+    criteria <-list(Subject=data4$Subject,Activity=data4$Activity,Dataset=data4$Dataset)
     data5 <- aggregate(data4[4:69], by=criteria, mean)
 }
 
 read_data5 <- function(datadir) {
     make_data5(read_data4(datadir))
 }
-    
+
+write_data5 <- function(data5, fname) {
+  write.table(arrange(data5,Subject,Activity),fname,row.names = FALSE)
+}
+
+read_write_data5 <- function(datadir, fname) {
+  write_data5(read_data5(datadir), fname)
+}
